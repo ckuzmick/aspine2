776ab03203d834f8ced48e03273544407f9dc538
ckuzmick <conrad.kuzmick@icloud.com>
2024-02-24 15:27:39 -0500

    stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,53 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function Home() {
-  const [sessionId, setSessionId] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
-    document.cookie = `JSESSIONID=${sessionId}`; // Set the cookie with the sessionId
-    window.location.href = '/dashboard';
+    const response = await fetch("/get_data/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    } else {
+      console.log("Login failed");
+    }
   };
 
   return (
     <main>
       <label>
-        Session ID:
-        <input type="text" name="session_id" value={sessionId} onChange={(e) => setSessionId(e.target.value)} />
+        Username:
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </label>
       <br />
-      <button type="submit" onClick={handleLogin}>Login</button>
+      <label>
+        Password:
+        <input
+          type="text"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <br />
+      <button type="submit" onClick={handleLogin}>
+        Login
+      </button>
     </main>
   );
 }
