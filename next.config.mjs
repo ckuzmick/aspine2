bd07a4c7f9920990220d06e68c5aebb52ac9cfed
ckuzmick <conrad.kuzmick@icloud.com>
2024-02-25 14:52:18 -0500

    stuff

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...config.externals, "playwright-core"];
+
+      // Handling for .ttf files
+      config.module.rules.push({
+        test: /\.ttf$/,
+        use: ["file-loader"],
+      });
+
+      // Optionally exclude html files from being processed
+      // This is more of a workaround and might not be necessary
+    }
+    return config;
+  },
+};
 
 export default nextConfig;
